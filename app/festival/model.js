ecd23c85faf747030b6201bd569f22acd4ccef18
callmelife <matthew.g.sommers@gmail.com>
2017-01-16 13:03:08 -0500

    Generate models for comment and festival

--- app/festival/model.js
+++ app/festival/model.js
@@ -3,11 +3,13 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
+  genre: DS.attr('string'),
   location: DS.attr('string'),
   venue: DS.attr('string'),
-  price: DS.attr('number'),
-  genre: DS.attr('string'),
   url: DS.attr('string'),
   date: DS.attr('string'),
-  comments: DS.attr(''),
+  price: DS.attr('number'),
+  hidden: DS.attr('boolean'),
+  comments: DS.hasMany('comment'),
+  // items: DS.hasMany('item'),
 });
