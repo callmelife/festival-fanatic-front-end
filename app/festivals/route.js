d9c48066073ac0fe254e5fd4c12e4531a04e3c3a
callmelife <matthew.g.sommers@gmail.com>
2017-01-18 18:23:50 -0500

    Generate festivals route and festivals template.
    Festivals are now able to be viewed in list from from all users via
    festivals/template. Festivals routes now correctly handles the
    actions passed to it.

--- app/festivals/route.js
+++ app/festivals/route.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.get('store').findAll('festival');
+  return this.get('store').findAll('festival');
+
   },
   actions: {
     deleteFestival (festival) {
